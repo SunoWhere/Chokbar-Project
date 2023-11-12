bd083ee82761a09aee1da6e4a6ec432f8873408b
Halm Robin <robin.halm68@gmail.com>
2023-11-12 14:28:32 +0100

    Addition of Sequelize ORM

---
+++ api/DBconnection.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('ezcon', 'root', 'toor', {
+    dialect:'postgres',
+    host:'localhost',
+    port:'5433'
+})
+
+sequelize.authenticate().then(() => {
+    console.log("Connection successful")
+}).catch((err) => {
+    console.log("Connection failed")
+})
+
+// sequelize.close().then(() => {
+//     console.log("Connection ended")
+// })
