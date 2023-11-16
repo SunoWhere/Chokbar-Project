b82bf285a20bf21e153f8e379e72347fc9d072df
Tom SIOUAN <tomsiouan@icloud.com>
2023-11-16 17:07:53 +0100

    Update app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -26,22 +26,22 @@ export default {
 
 :root {
   /* Green variations */
-  --c-light-green: var(#9CFC97);
-  --c-asparagus: var(#6BA368);
-  --c-dark-moss-green: var(#515B3A);
-  --c-black-olive: var(#353D2F);
-  --c-ancient-green: var(#6D9777);
+  --c-light-green: #9CFC97;
+  --c-asparagus: #6BA368;
+  --c-dark-moss-green: #515B3A;
+  --c-black-olive: #353D2F;
+  --c-ancient-green: #6D9777;
 
   /* Buttons */
-  --c-grey-button: var(#1E1E1E);
+  --c-grey-button: #1E1E1E;
 
   /* Background colors */
-  --c-black-background: var(#0C0F0B);
+  --c-black-background: #0C0F0B;
 
   /* Crud colors */
-  --c-crud-bleu: var(#006EF0);
-  --c-crud-rouge: var(#D20000);
-  --c-crud-vert: var(#00BF6F);
+  --c-crud-bleu: #006EF0;
+  --c-crud-rouge: #D20000;
+  --c-crud-vert: #00BF6F;
 }
 
 * {
@@ -52,22 +52,5 @@ export default {
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
