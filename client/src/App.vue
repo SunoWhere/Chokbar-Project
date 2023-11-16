4c3e0765bc709f106fe99e1c854ae235f6ce45c4
Tom SIOUAN <tomsiouan@icloud.com>
2023-11-16 17:03:28 +0100

    Mise en place de variables de couleur.

--- client/src/App.vue
+++ client/src/App.vue
@@ -24,6 +24,26 @@ export default {
 
 <style>
 
+:root {
+  /* Green variations */
+  --c-light-green: var(#9CFC97);
+  --c-asparagus: var(#6BA368);
+  --c-dark-moss-green: var(#515B3A);
+  --c-black-olive: var(#353D2F);
+  --c-ancient-green: var(#6D9777);
+
+  /* Buttons */
+  --c-grey-button: var(#1E1E1E);
+
+  /* Background colors */
+  --c-black-background: var(#0C0F0B);
+
+  /* Crud colors */
+  --c-crud-bleu: var(#006EF0);
+  --c-crud-rouge: var(#D20000);
+  --c-crud-vert: var(#00BF6F);
+}
+
 * {
   margin: 0;
   padding: 0;
