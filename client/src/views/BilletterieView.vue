0c1aa86076d7c0435aec1c95b35f981d75751c5f
Lucas Da Silveira <lucasdslvra@gmail.com>
2023-11-22 15:05:58 +0100

    Billetterie 99% finito

--- client/src/views/BilletterieView.vue
+++ client/src/views/BilletterieView.vue
@@ -23,41 +23,61 @@ export default {
           </div>
           <div class="price-section">
             <strong class="price">
-              <strong>10€</strong>
+              <strong>10</strong>
+              <span>€</span>
             </strong>
           </div>
-          <a href="#" class="Choose-plan-btn">Réserver <i class="fas fa-chevron-right"></i></a>
+          <a href="#" class="Choose-plan-btn">Réserver</a>
         </div>
         <div class="box-features-section">
-          <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>60-day chat history</span>
-            <div class="feature-details">
-              <p>Your chats will be lo stored in the archives for 60 days.</p>
+          <div class="box-features-title">
+            <span>Avantage</span>
             </div>
+          <div class="features-box">
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
+            </div>
+            <span>Événements</span>
           </div>
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Basic widget customization</span>
-            <div class="feature-details">
-              <p>Customization is limited to changing the theme and colors of the chat widget.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Concours</span>
           </div>
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Ticketing system</span>
-            <div class="feature-details">
-              <p>Built-in ticketing system that will allow you to create, manage, and track tougher cases. You can also use it to handle emails and messages left when your LiveChat is offline.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Commande en ligne</span>
+          </div>
+          <div class="box-features-title">
+            <p>Extra</p>
           </div>
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Data security</span>
-            <div class="feature-details">
-              <p>Secure 256-bit SSL encryption for website visitors and agents connecting to your LiveChat.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Goodies</span>
           </div>
-
+          <div class="features-box">
+            <div class="cross-icon">
+              <font-awesome-icon icon="fa-solid fa-xmark" />
+            </div>
+            <span>File prioritaire</span>
+          </div>
+          <div class="features-box">
+            <div class="cross-icon">
+              <font-awesome-icon icon="fa-solid fa-xmark" />
+            </div>
+            <span>Espace VIP</span>
+          </div>
+          <div class="features-box">
+            <div class="cross-icon">
+              <font-awesome-icon icon="fa-solid fa-xmark" />
+            </div>
+            <span>Accès aux avants premières</span>
+          </div>
         </div>
       </div>
 
@@ -72,39 +92,60 @@ export default {
           </div>
           <div class="price-section">
             <strong class="price">
-              <strong>45€</strong>
+              <strong>45</strong>
+              <span>€</span>
             </strong>
           </div>
-          <a href="#" class="Choose-plan-btn">Réserver <i class="fas fa-chevron-right"></i></a>
+          <a href="#" class="Choose-plan-btn">Réserver </a>
         </div>
         <div class="box-features-section">
-          <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>60-day chat history</span>
-            <div class="feature-details">
-              <p>Full customization, including changing the theme, colors, visibility.</p>
-            </div>
+          <div class="box-features-title">
+            <p>Avantage</p>
           </div>
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Basic widget customization</span>
-            <div class="feature-details">
-              <p>Built-in ticketing system that will allow you to create, manage, and track tougher cases. You can also use it to handle emails and messages left when your LiveChat is offline.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Événement</span>
           </div>
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Ticketing system</span>
-            <div class="feature-details">
-              <p>Secure 256-bit SSL encryption for website visitors and agents connecting to your LiveChat.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Concours</span>
           </div>
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Data security</span>
-            <div class="feature-details">
-              <p>Chat and ticket reports plus a daily recap of your account activity.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Commande en ligne</span>
+          </div>
+          <div class="box-features-title">
+            <p>Extra</p>
+          </div>
+            <div class="features-box">
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
+            </div>
+            <span>Goodies</span>
+          </div>
+          <div class="features-box">
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
+            </div>
+            <span>File prioritaire</span>
+          </div>
+          <div class="features-box">
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
+            </div>
+            <span>Espace VIP</span>
+          </div>
+          <div class="features-box">
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
+            </div>
+            <span>Accès aux avants premières</span>
           </div>
         </div>
       </div>
@@ -117,40 +158,60 @@ export default {
           </div>
           <div class="price-section">
             <strong class="price">
-              <strong>25€</strong>
+              <strong>25</strong>
+              <span>€</span>
             </strong>
           </div>
           <a href="#" class="Choose-plan-btn">Réserver<i class="fas fa-chevron-right"></i></a>
         </div>
         <div class="box-features-section">
-          <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>60-day chat history</span>
-            <div class="feature-details">
-              <p>Full customization, including changing the theme, colors, visibility.</p>
-            </div>
+          <div class="box-features-title">
+            <p>Avantage</p>
           </div>
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Basic widget customization</span>
-            <div class="feature-details">
-              <p>Built-in ticketing system that will allow you to create, manage, and track tougher cases. You can also use it to handle emails and messages left when your LiveChat is offline.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Événements</span>
           </div>
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Ticketing system</span>
-            <div class="feature-details">
-              <p>Secure 256-bit SSL encryption for website visitors and agents connecting to your LiveChat.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Concours</span>
           </div>
-
           <div class="features-box">
-            <i class="fas fa-check"></i>
-            <span>Multiple brandings</span>
-            <div class="feature-details">
-              <p>All your chats will be stored in the archives.</p>
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
             </div>
+            <span>Commande en ligne</span>
+          </div>
+          <div class="box-features-title">
+            <p>Extra</p>
+          </div>
+          <div class="features-box">
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
+            </div>
+            <span>Goodies</span>
+          </div>
+          <div class="features-box">
+            <div class="check-icon">
+              <font-awesome-icon icon="fa-solid fa-check" />
+            </div>
+            <span>File prioritaire</span>
+          </div>
+          <div class="features-box">
+            <div class="cross-icon">
+              <font-awesome-icon icon="fa-solid fa-xmark" />
+            </div>
+            <span>Espace VIP</span>
+          </div>
+          <div class="features-box">
+            <div class="cross-icon">
+              <font-awesome-icon icon="fa-solid fa-xmark" />
+            </div>
+            <span>Accès aux avants premières</span>
           </div>
         </div>
       </div>
@@ -159,24 +220,34 @@ export default {
 </template>
 
 <style>
+
+
 a{
   text-decoration: none;
 }
-#pricing-table{
+
+section{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 80px);
+}
+
+.box-top-section{
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  width: 100%;
-  min-height: 100vh;
-  //background-color: #0C0F0B;
+  min-height: 100px;
 }
+
 .price-box-container{
   display: flex;
   justify-content: center;
   margin-top: 30px;
   flex-wrap: wrap;
 }
+
 .price-box{
   margin: 10px;
   width: 265px;
@@ -227,12 +298,21 @@ a{
   color: #19181e;
 }
 .price span{
-  font-size: 0.8rem;
+  font-size: 2rem;
   padding: 0px;
   margin: 0px;
   color: #292929;
   font-weight: 500;
 }
+
+.box-features-title {
+  font-weight: 700;
+  width: 100%;
+  display: flex;
+  padding: 10px 20px;
+  position: relative;
+}
+
 .box-features-section{
   padding: 20px 10px;
   display: flex;
@@ -256,11 +336,20 @@ a{
 
 }
 
-.features-box i{
-  color: #462ebd;
+.check-icon{
+  display: flex;
+  color: var(--c-crud-vert);
   margin-right: 7px;
   font-size: 0.9rem;
 }
+
+.cross-icon{
+  display: flex;
+  color: var(--c-crud-rouge);
+  margin-right: 7px;
+  font-size: 0.9rem;
+}
+
 .features-box span{
   color: #242424;
   font-size: 0.8rem;
@@ -270,38 +359,6 @@ a{
   transition: all ease 0.3s;
 }
 
-.feature-details{
-  position: absolute;
-  bottom:45px;
-  left: 50%;
-  transform: translateX(-50%);
-  width:100%;
-  height: auto;
-  padding: 10px;
-  background-color: #242424;
-  color: #ebebeb;
-  border-radius: 8px;
-  font-size: 0.8rem;
-  text-align: center;
-  box-shadow: 2px 2px 30px rgba(0,0,0,0.10);
-  display: none;
-  animation: fade 0.2s;
-}
-.feature-details:after{
-  content: '';
-  width:0px;
-  height: 0px;
-  border-bottom: 5px solid transparent;
-  border-left: 7px solid transparent;
-  border-top: 7px solid #242424;
-  border-right: 5px solid transparent;
-  position: absolute;
-  left: 50%;
-  top: 100%;
-}
-.features-box:hover .feature-details{
-  display:block;
-}
 .Choose-plan-btn{
   margin-top: 20px;
   font-size: 15px;
@@ -350,7 +407,7 @@ a{
 }
 .price-box:hover .box-top-section,
 .price-box:hover .box-features-section{
-  box-shadow: 2px 2px 30px rgba(0,0,0,0.09);
+  box-shadow: 2px 2px 30px rgba(255, 255, 255, 0.5);
   transition: all ease 0.2s;
 }
 @media(max-width:1160px){
