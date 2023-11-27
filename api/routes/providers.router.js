e4df6ec8be125db6352f7755685a9c483c2ab46b
Halm Robin <robin.halm68@gmail.com>
2023-11-27 13:02:50 +0100

    CRUD providers

--- api/routes/providers.router.js
+++ api/routes/providers.router.js
@@ -4,12 +4,22 @@ const providersMiddleware = require("../middlewares/providers.middleware")
 var router = express.Router()
 
 router.get("/", providersController.getProviders)
-router.get("/:id")
+router.get("/:id", providersMiddleware.validateId, providersController.getProviderById)
 
-router.post("/")
+router.post("/", providersMiddleware.validateProviderInput, providersController.saveProvider)
 
-router.put("/:id")
+router.put("/:id", providersMiddleware.validateId, providersMiddleware.validateProviderInput, providersController.updateProviderById)
 
-router.delete("/:id")
+router.delete("/:id", providersMiddleware.validateId, providersController.deleteProviderById)
+
+// TODO : rajouter les routes pour gérer les produits et article
+router.get("/:id/")
+router.get("/:id/products/")
+router.get("/:id/products/:id_product")
+
+router.post("/:id/products/")
+router.put("/:id/products/:id_product")
+
+router.delete("/:id/products/:id_product")
 
 module.exports = router
