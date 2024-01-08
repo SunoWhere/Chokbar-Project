b99203dc175799bd113120b927b67f72834af78c
Tom Siouan <tomsiouan@icloud.com>
2024-01-09 00:39:58 +0100

    Provider crud version 1

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,39 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {usersService} from "@/services";
+import {providersService, usersService} from "@/services";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    showMapTooltip: false,
+    isMapTooltipOpen: false,
     showPlanningInfo: false,
     showAddEvent: false,
 
     isConnected: usersService.getUuid() != null,
     role: usersService.getRole(),
 
-    users: await usersService.getAllUser(), // All users of the base
+    users: [],
+    providers: [],
+
     userIdToRemove: null,
+    providerIdToRemove: null,
     userToEdit: null,
 
     showAddUserPopup: false,
     showEditUserPopup: false,
     showRemoveUserPopup: false,
+
+    showNewClientOrNotPopup: false,
+    showAddProviderPopup: false,
+    showLinkClientProviderPopup: false,
+    showRemoveProviderPopup: false,
   },
   getters: {
     getUserList: state => state.users,
-    getShowMapTooltip: state => state.showMapTooltip,
+    getProviderList: state => state.providers,
+
     getShowPlanningInfo: state => state.showPlanningInfo,
     getShowAddEvent: state => state.showAddEvent,
   },
   mutations: {
+    // Connection
     setConnected(state, value) {
       state.isConnected = value;
     },
     setRole(state, value) {
       state.role = value;
     },
+
+    // Users
     setShowAddUserPopup(state, value) {
       state.showAddUserPopup = value;
     },
@@ -43,14 +55,11 @@ export default new Vuex.Store({
     setUserList(state, userList) {
       state.users = userList;
     },
+    setProviderList(state, providerList) {
+      state.providers = providerList;
+    },
     setUserIdToRemove(state, value) {
       state.userIdToRemove = value;
-    },
-    setShowPlanningInfo(state, value) {
-      state.showPlanningInfo = value;
-    },
-    setShowAddEvent(state, value) {
-      state.showAddEvent = value;
     },
     setUserToEdit(state, value) {
       state.userToEdit = value;
@@ -58,8 +67,30 @@ export default new Vuex.Store({
     setShowEditUserPopup(state, value) {
       state.showEditUserPopup = value;
     },
-    setShowMapTooltip(state, value) {
-      state.showMapTooltip = value;
+
+    // Planning / Event
+    setShowPlanningInfo(state, value) {
+      state.showPlanningInfo = value;
+    },
+    setShowAddEvent(state, value) {
+      state.showAddEvent = value;
+    },
+
+    // Providers
+    setProviderIdToRemove(state, value) {
+      state.providerIdToRemove = value;
+    },
+    setShowNewClientOrNotPopup(state, value) {
+      state.showNewClientOrNotPopup = value;
+    },
+    setShowAddProviderPopup(state, value) {
+      state.showAddProviderPopup = value;
+    },
+    setShowLinkClientProviderPopup(state, value) {
+      state.showLinkClientProviderPopup = value;
+    },
+    setShowRemoveProviderPopup(state, value) {
+      state.showRemoveProviderPopup = value;
     },
   },
   actions: {
@@ -67,6 +98,14 @@ export default new Vuex.Store({
       try {
         const userList = await usersService.getAllUser();
         commit('setUserList', userList.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async updateProviderList({ commit }) {
+      try {
+        const providerList = await providersService.getAllProvider();
+        commit('setProviderList', providerList.data);
       } catch (error) {
         console.error(error);
       }
