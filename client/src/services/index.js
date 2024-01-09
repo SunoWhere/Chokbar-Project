85c0e4f62841adeae5bf789364ec2660650a52e3
Lucas Da Silveira <lucasdslvra@gmail.com>
2024-01-09 08:44:08 +0100

    update

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -4,4 +4,5 @@ Axios.defaults.baseURL = 'http://localhost:8081';
 export default Axios;
 export * from './users.service';
 export * from './events.service';
+export * from './providers.service';
 export * from './stands.service';
