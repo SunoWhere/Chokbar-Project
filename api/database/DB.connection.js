fdf1c3bbd11462f95ed4b882819298cc1aab98f1
Jessy <sunowhere@gmail.com>
2024-01-11 21:51:30 +0100

    Init DB and Tickets

--- api/database/DB.connection.js
+++ api/database/DB.connection.js
@@ -13,14 +13,51 @@ const sequelize = new Sequelize('ezcon', DB_USER, DB_PWD, {
 
 const DB_models = require("./models/init-models").initModels(sequelize)
 
+const initTickets = () => {
+    try{
+        const alphanum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_'
+        for(let i = 0; i < 300; i++) {
+            const id_type_ticket = Math.floor(Math.random() * 3 + 1)
+            const mail = 'user' + i + '@mail.to'
+            let hash = ''
+            for (let j = 0; j < 256; j++) {
+                hash += alphanum.charAt(Math.floor(Math.random() * alphanum.length));
+            }
+            DB_models.tickets.create({
+                hash: hash,
+                email: mail,
+                id_ticket_type: id_type_ticket
+            })
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const initProducts = () => {
+    try{
+        for(let i = 0; i < 300; i++) {
+            const id_type_ticket = Math.floor(Math.random() * 3 + 1)
+            DB_models.o.create({
+                hash: hash,
+                email: mail,
+                id_ticket_type: id_type_ticket
+            })
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 const initDb = () => {
     sql_script = fs.readFileSync(__dirname + '/bdd.sql').toString()
     sequelize.query(sql_script)
+    initTickets()
 }
 
 sequelize.authenticate().then(() => {
-    console.log("Connection successful")
     initDb()
+    console.log("Connection successful")
 }).catch((err) => {
     console.log("Connection failed")
 })
