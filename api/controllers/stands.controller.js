b35bee201f3a59f790bb4548595534005693760a
Halm Robin <robin.halm68@gmail.com>
2024-04-03 14:41:11 +0200

    quick fix

--- api/controllers/stands.controller.js
+++ api/controllers/stands.controller.js
@@ -92,15 +92,17 @@ exports.deleteStandById = async (req, res) => {
 }
 exports.saveStand = async (req, res) => {
     try {
-        await standsServices.saveStand(
-            req.body.id_location,
-            req.body.id_provider,
-            req.body.id_stand_type,
-            req.body.name,
-            req.body.description_en,
-            req.body.description_fr
+
+        return res.status(200).send(
+            await standsServices.saveStand(
+                req.body.id_location,
+                req.body.id_provider,
+                req.body.id_stand_type,
+                req.body.name,
+                req.body.description_en,
+                req.body.description_fr
+            )
         )
-        return res.status(200).send("New stand saved successfully.")
     } catch (err) {
         return res.status(500).send(err.message)
     }
